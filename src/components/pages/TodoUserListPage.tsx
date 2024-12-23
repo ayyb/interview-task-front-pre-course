441f0f7ddbc4790849afe1468de613b7630b51cb
이명섭 <abbbc@mz.co.kr>
2024-12-23 21:00:42 +0900

    feat: 초기 레이아웃 구조 설계

--- src/components/pages/TodoUserListPage.tsx
+++ src/components/pages/TodoUserListPage.tsx
@@ -9,7 +9,30 @@ interface Props {}
 const TodoUserListPage = ({}: Props) => {
   return (
     <Container>
-      <div>todo</div>
+      <div><h1>To Do List</h1></div>
+
+      {/* input */}
+      <div>
+        <input type="text" />
+      </div>
+      {/* board */}
+      <div>
+        {/* tab */}
+        <div>
+          <div>ALL</div>
+          <div>TODO</div>
+          <div>DONE</div>
+        </div>
+        {/* list */}
+        <div>
+          <span> 총 3건</span>
+          <div>
+            <div><input type="checkbox"/>출근하고 비타민 먹기<span>x</span></div>
+            <div><input type="checkbox"/>Daily Scrum 작성하기<span>x</span></div>
+            <div><input type="checkbox"/>주간회의 참여하기<span>x</span></div>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
